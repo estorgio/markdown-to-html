f7a8cacbdb85effed7ee65a8c9ac21c81e3854b9
Fortunato Estorgio <contact@estorgio.com>
2019-02-17 21:09:31 +0800

    Move html and css template to 'assets' directory

--- index.js
+++ index.js
@@ -65,9 +65,9 @@ async function copyImageToOutput(filePath) {
 }
 
 function loadTemplate(content) {
-  const html = fs.readFileSync(path.resolve(__dirname, 'template.html'), 'utf-8');
-  const css = fs.readFileSync(path.resolve(__dirname, 'style.css'), 'utf8');
-  const githubSyntaxHighlight = fs.readFileSync(path.resolve(__dirname, 'github-gist.css'), 'utf8');
+  const html = fs.readFileSync(path.resolve(__dirname, 'assets', 'template.html'), 'utf-8');
+  const css = fs.readFileSync(path.resolve(__dirname, 'assets', 'style.css'), 'utf8');
+  const githubSyntaxHighlight = fs.readFileSync(path.resolve(__dirname, 'assets', 'github-gist.css'), 'utf8');
   const $source = $.load(html);
   $source('head').append(`<style>${githubSyntaxHighlight}</style>`);
   $source('head').append(`<style>${css}</style>`);
